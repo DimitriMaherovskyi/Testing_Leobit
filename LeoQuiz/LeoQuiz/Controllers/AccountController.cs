d2a4f9d07b257fc0db12f630a32e598154662f7c
Dmytro Maherovskyi <xxmdvxx@gmail.com>
2016-09-29 21:28:44 +0300

    Authefication stuff

---
+++ LeoQuiz/LeoQuiz/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LeoQuiz.Entities;
+using System.Web.Security;
+
+namespace LeoQuiz.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                string username = model.Name;
+                string password = model.Password;
+
+                bool userValid = username == "admin" && password == "1234";
+
+                if (userValid)
+                {
+                    FormsAuthentication.SetAuthCookie(username, false);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            return View(model);
+        }
+
+        
+    }
+}
---
+++ LeoQuiz/LeoQuiz/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LeoQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        //public string Index()
+        //{
+        //    return "Hello MVC";
+        //}
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This can be viewed only by authenticated users only";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
